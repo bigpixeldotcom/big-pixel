14768afa10de6e14c6d8d3ae817dc7252b96e643
James Beston <james@beston.me>
2024-11-10 16:41:16 +0000

    Update website-spline.tsx

--- components/digital/website-spline.tsx
+++ components/digital/website-spline.tsx
@@ -2,7 +2,7 @@ import Spline from '@splinetool/react-spline/next';
 
 export default function WebsiteSpline() {
   return (
-    <div className='relative w-auto h-auto'>
+    <div className='relative w-[312px] h-[240px] md:w-[660px] md:h-[420px] lg:w-[896px] lg:h-[672px]'>
       <Spline
         scene="https://prod.spline.design/XfimQlRAOCx7vvlB/scene.splinecode" 
       />
