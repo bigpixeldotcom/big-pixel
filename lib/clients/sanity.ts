4918823e5bf23ccbf361a6ba1152652942f08a36
James Beston <james@beston.me>
2024-11-21 10:09:24 +0000

    Update sanity.ts

--- lib/clients/sanity.ts
+++ lib/clients/sanity.ts
@@ -1,9 +1,9 @@
 import { createClient } from 'next-sanity';
 
 export const sanityClient = createClient({
-  projectId: "4ubrnd3n",
-  dataset: "development",
+  projectId: process.env.SANITY_PROJECT_ID!,
+  dataset: process.env.SANITY_DATASET!,
   apiVersion: "2022-03-07",
-  token: process.env.SANITY_API_READ_TOKEN,
+  token: process.env.SANITY_API_READ_TOKEN!,
   useCdn: false,
 });
