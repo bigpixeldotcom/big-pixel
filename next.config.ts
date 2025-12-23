13424493544412ebd8a9d79e83ed76635d26e0ea
adrian-beston <adrian@big-pixel.com>
2025-12-23 16:26:23 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,21 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import createMDX from '@next/mdx';
 
 const nextConfig: NextConfig = {
   /* config options here */
-    async rewrites() {
-    		return [
-    			{
-    				source: '/api/c15t/:path*',
-    				destination: `${process.env.NEXT_PUBLIC_C15T_URL}/:path*`,
-    			},
-    		];
-    	}
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  async rewrites() {
+    return [
+      {
+        source: '/api/c15t/:path*',
+        destination: `${process.env.NEXT_PUBLIC_C15T_URL}/:path*`,
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+});
+
+export default withMDX(nextConfig);
